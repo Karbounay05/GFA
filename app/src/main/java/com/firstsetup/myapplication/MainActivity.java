96d409fbfd124fe8948d030caf47c4428fb805ab
Karbounay05 <karbounay05@gmail.com>
2025-03-30 22:01:33 +0200

    Normalize line endings to prevent CRLF warnings

--- app/src/main/java/com/firstsetup/myapplication/MainActivity.java
+++ app/src/main/java/com/firstsetup/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.firstsetup.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,10 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Find the "S'inscrire" button and set an onClick listener
+        Button signupButton = findViewById(R.id.button2);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open Page1Activity when button is clicked
+                Intent intent = new Intent(MainActivity.this, Page1Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/firstsetup/myapplication/Page1Activity.java
@@ -0,0 +1,22 @@
+package com.firstsetup.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Page1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page1);  // This links to page1.xml
+
+
+
+
+
+    }
+
+
+}
