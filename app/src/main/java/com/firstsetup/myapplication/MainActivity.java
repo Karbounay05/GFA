81f6d9247943680ee9940d948055a41e062b91e3
Karbounay05 <karbounay05@gmail.com>
2025-04-04 13:27:03 +0200

    create a slide pages

--- app/src/main/java/com/firstsetup/myapplication/MainActivity.java
+++ app/src/main/java/com/firstsetup/myapplication/MainActivity.java
@@ -1,20 +1,24 @@
 package com.firstsetup.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
 
-        // Start IntroActivity and finish MainActivity
-        Intent intent = new Intent(MainActivity.this, IntroActivity.class);
-        startActivity(intent);
-        finish(); // Close MainActivity to prevent looping back
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+
+        List<String> slides = Arrays.asList("Slide 1", "Slide 2", "Slide 3");
+        SlideAdapter adapter = new SlideAdapter(slides);
+        viewPager.setAdapter(adapter);
     }
 }
