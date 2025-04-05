bab20d04b6a35aefc5892dc95aff4fec4a1706c4
pc-le <alinouar160@gmail.com>
2025-04-05 16:46:13 +0000

    j'ai corriger une erreur dans la classe ApiServer

--- app/src/main/java/com/firstsetup/myapplication/ApiServer.java
+++ app/src/main/java/com/firstsetup/myapplication/ApiServer.java
@@ -1,7 +1,10 @@
 package com.firstsetup.myapplication;
 
     import retrofit2.Call;
+    import retrofit2.http.Body;
     import retrofit2.http.GET;
+    import retrofit2.http.POST;
+
     import java.util.List;
 
     public class ApiServer {
