f85b170dfc6aaa5e21479fe12ba005003fa94f4a
pc-le <alinouar160@gmail.com>
2025-04-15 14:18:58 +0100

    j'ai ajouté la meteo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.core.ktx)
+    implementation(libs.androidx.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
